19b9a630f44b79286b9a0264ea99287cdf3acca5
Blaize Berry <blaizeberry@Blaizes-MacBook-Pro.local>
2024-01-01 16:28:52 -0600

    remove full width

--- app/components/TournamentCard.tsx
+++ app/components/TournamentCard.tsx
@@ -28,7 +28,7 @@ export const TournamentCard: React.FC<TournamentCardProps> = ({ tournament, onCl
 
     return (
         <a href="#" className="flex flex-row items-center bg-white border border-gray-200 rounded-lg shadow md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img className="object-cover w-full rounded-full p-2 h-16 w-16 md:h-36 md:w-36" src={tournament.tournamentLogo} alt="" />
+            <img className="object-cover rounded-full p-2 h-36 w-36 md:h-36 md:w-36" src={tournament.tournamentLogo} alt="" />
             <div className="flex flex-col justify-between p-2 leading-normal">
                 <h5 className="mb-1 text-sm md:text-lg font-bold tracking-tight text-gray-900 dark:text-white">{tournament.tournamentName}</h5>
                 <p className="mb-1 text-xs md:text-md font-normal text-gray-700 dark:text-gray-400">{tournament.date}<br/>{location}</p>
