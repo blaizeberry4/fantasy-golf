18a231b7e87d41c2db3d30552102c386b9aca204
Blaize Berry <blaizeberry@Blaizes-MBP.attlocal.net>
2024-01-03 14:03:14 -0600

    add manifest

---
+++ app/manifest.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'PUP Golf',
+    short_name: 'PUP Golf',
+    description: 'A Fantasy Golf League for PUPs',
+    start_url: '/events',
+    display: 'fullscreen',
+    orientation: 'portrait',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
